acd67bc52df408fb7dd9c9b5bfcfdbd092784ff8
md <md@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2007-10-21 20:56:03 +0000

    komissioniersimulation deutlich verbessert
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/trunk/web/MoftS/lib/myPL/kernelE@2451 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- test/play_vorgaenge.py
+++ test/play_vorgaenge.py
@@ -1,6 +1,7 @@
+# -*- encoding: utf-8 -*-
 # spielt von dump_softm.py gespeicherte Vorgaenge gegen das myPL ab.
 # Dies ist eigentlich eine suimulationsengine
-import sys, pickle, gzip, datetime
+import sys, pickle, gzip, datetime, time
 sys.path.extend(['./python'])
 from kernelE import Kerneladapter
 
@@ -14,97 +15,120 @@ class Orderline(object):
 
 globalstats = {}
 
-def process_orders(auftragsnummern):
-    global byauftrag
-    global globalstats
-    nofit = []
-    stats = {}
-    
-    def statnote(name, value):
-        if not name in stats:
-            stats[name] = 0
-        stats[name] = stats[name] + value
-        if not name in globalstats:
-            globalstats[name] = 0
-        globalstats[name] = globalstats[name] + value
-    
-    k = Kerneladapter()
-    for auftragsnummer in auftragsnummern:
-        # here we iterate over all auftrag items 
-        positionen = byauftrag[auftragsnummer]
-        # print auftragsnummer, len(positionen)
-        # print auftragsnummer, len(positionen), [(pos.menge, pos.artnr) for pos in positionen]
-        has_movements = False
-        # print "k.init_provisionings_multi(%r)" % ([(pos.menge, pos.artnr) for pos in positionen])
-        ret = k.init_provisionings_multi([(pos.menge, pos.artnr) for pos in positionen])
-        if ret[0] == 'error':
-            print ret, [(pos.menge, pos.artnr) for pos in positionen]
-            statnote('retried_orders', 1)
-            nofit.append(auftragsnummer)
-        else:
-            statnote('finished_orders', 1)
-            # commit all the stuiff
-            ok, movements, picks = ret
-            for mov in movements:
-                has_movements = True
-                statnote('retrievals', 1)
-                # print "commit", mov, 
-                k.commit_movement(mov)
-            for pick in picks:
-                statnote('picks', 1)
-                # print "commit", pick, 
-                k.commit_pick(pick)
+class Lieferung(object):
+    def __init__(self, oid, kundennummer, liefertermin, positionen):
+        self.oid = oid
+        self.kundennummer = kundennummer
+        self.liefertermin = liefertermin
+        self.positionen = positionen
         
-        if not has_movements:
-            movements = k.create_automatic_movements()
-            for mov in movements:
-                # print "commit", mov, 
-                k.commit_movement(mov)
-                statnote('automovements', 1)
-    
-    # print nofit
-    print "nofits left", len(set(nofit))
-    print "statistics, auftraege=", len(auftragsnummern), stats
-    return nofit
-    
 
+class PlayerOfOrders(object):
+    """this is a prototype for interacting with kernel-E, specifically the processing of orders/provisionings"""
+    
+    def __init__(self):
+        self.lieferungen_done = set()
+        self.lieferungen_todo = []
+        self.open_picks = []
+        self.open_retrievals = []
+        self.open_movements = []
+        self.picks_per_round = 8
+        self.retrievals_per_round = 2
+        self.movements_per_round = 1
+        self.stats = {}
+    
+    def add_lieferungen(self, lieferungen):
+        """Neue Lieferungen der Engine bekannt machen."""
+        for lieferung in lieferungen:
+            if lieferung not in self.lieferungen_done:
+                self.lieferungen_todo.append(lieferung)
+        
+    def statnote(self, name, value):
+        if not name in self.stats:
+            self.stats[name] = 0
+        self.stats[name] = self.stats[name] + value
+    
+    def simulate(self):
+        """Simuliert Lagerbewegingen. Hierbei wird simuliert jede Kommibeleg Position würde unabhaengig von den anderen zurueckgemeldet."""
+        k = Kerneladapter()
+        thisround = 0
+        while self.lieferungen_todo:
+            
+            # everybody loves statistics
+            thisround += 1
+            print "Round %d: todo:%d picks:%d retrievals:%d movements:%d" % (thisround, 
+                   len(self.lieferungen_todo), len(self.open_picks), len(self.open_retrievals),
+                   len(self.open_movements))
+            print self.stats
+            time.sleep(1)
+            
+            # process picks
+            for i in range(self.picks_per_round):
+                if self.open_picks:
+                    pickId = self.open_picks.pop(0)
+                    k.commit_pick(pickId)
+                    self.statnote('picks', 1)
+            
+            # process retrievals (= "picks for full pallets")
+            for i in range(self.retrievals_per_round):
+                if self.open_retrievals:
+                    retrievalId = self.open_retrievals.pop(0)
+                    k.commit_movement(retrievalId) # FIXME: this actually leaves the goods on AUSLAG
+                    self.statnote('retrievals', 1)
+            
+            # process movements ("umlagerungen")
+            for i in range(self.movements_per_round):
+                if self.open_movements:
+                    movementId = self.open_movements.pop(0)
+                    k.commit_movement(movementId)
+                    self.statnote('movements', 1)
+            
+            #### now generate new picks/retrievals (unles both queues are still full)
+            init_provisionings_multi_count = 0
+            while ((not self.open_picks) or (not self.open_retrievals) 
+                   and (len(self.open_picks) <= 4*self.picks_per_round 
+                        and len(self.open_retrievals) <= 4*self.retrievals_per_round)):
+                lieferung = self.lieferungen_todo.pop(0)
+                ret = k.init_provisionings_multi([(pos.menge, pos.artnr) for pos in lieferung.positionen])
+                if ret[0] == 'error':
+                    # can't be retrived at the moment
+                    #  print ret, [(pos.menge, pos.artnr) for pos in positionen]
+                    self.statnote('retried_orders', 1)
+                    self.lieferungen_todo.append(lieferung)
+                else:
+                    self.statnote('finished_orders', 1)
+                    ok, retrievals, picks = ret
+                    self.open_picks.extend(picks)
+                    self.open_retrievals.extend(retrievals)
+                    self.lieferungen_done.add(lieferung)
+                    self.stats['toppicksper'] = max([self.stats['toppicksper'], len(picks)])
+                    self.stats['topretrievalsper'] = max([self.stats['topretrievalsper'], len(retrievals)])
+                
+                # escape from deadlocks
+                init_provisionings_multi_count += 1
+                if init_provisionings_multi_count > 10:
+                    break
+            
+            # movements to refill floorlevel
+            if not self.open_movements:
+                movements = k.create_automatic_movements()
+                self.open_movements.extend(movements)
+            
+            
+    
 def main():
     oldnofit = []
     print "Simulating a day of myPL at work"
     vorgaenge = pickle.load(gzip.GzipFile('test/data/vorgaenge-20071019.pickle.gz', 'r'))
     for v in vorgaenge:
         o = Orderline()
-        o.menge, o.artnr, o.auftragsnummer, o.liefer_date =  v['menge'], v['artnr'], v['auftragsnummer'], v['liefer_date']
+        o.menge, o.artnr, o.auftragsnummer, o.liefer_date, o.kundennummer, o.vorgangsnummer = v['menge'], v['artnr'], v['auftragsnummer'], v['liefer_date'], v['kundennummer'], v['vorgangsnummer']
         byauftrag.setdefault(o.auftragsnummer, []).append(o)
-        bydate.setdefault(o.liefer_date, set()).add(o.auftragsnummer)
-    days = sorted(bydate.keys())
-    for day in days:
-        print day
-        auftragsnummern = bydate[day]
-        nofit = process_orders(auftragsnummern)
-        if nofit:
-            print "nofit", len(nofit)
-            oldnofit.extend(process_orders(nofit))
-    
-    print "******************** Final"
-    print "nofits left", len(set(oldnofit))
-    print "statistics", globalstats
-    print "last run"
-    oldnofit = process_orders(list(set(oldnofit)))
-    print "nofits left", len(set(oldnofit))
-    print "statistics", globalstats
-    print oldnofit, [byauftrag[auftragsnummer] for auftragsnummer in oldnofit]
-    
-    for auftragsnummer in oldnofit:
+    sim = PlayerOfOrders()
+    for auftragsnummer in byauftrag.keys():
         positionen = byauftrag[auftragsnummer]
-        print "mypl_provisioning:find_provisioning_candidates_multi([%s])" % ','.join(["{%s,%s}" % (pos.menge, pos.artnr) for pos in positionen])
-    
-    movements = True
-    k = Kerneladapter()
-    while movements:
-        movements = k.create_automatic_movements()
-        for mov in movements:
-            print mov
-            # print "commit", mov, k.commit_movement(mov)
+        l = Lieferung(positionen[0].vorgangsnummer, positionen[0].kundennummer, positionen[0].liefer_date, positionen)
+        sim.add_lieferungen([l])
+    sim.simulate()
 
 main()
--- test/reinit_lager.sh
+++ test/reinit_lager.sh
@@ -3,6 +3,6 @@
 rm -Rf Mnesia.mypl_test@*
 erl -pa ./ebin ./vendor/eunit/ebin -sname mypl_test -noinput \
     -s mypl_db run_me_once \
-    -s mnesia load_textfile '"test/data/lager-20071019.mnesiadump"' \
-    -s mnesia info \
+    -eval "mnesia:load_textfile(\"test/data/lager-20071019.mnesiadump\")" \
+    -s mnesia stop \
     -s erlang halt
