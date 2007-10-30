d7c9048d86ea522f6445bdc080f008910e4c0095
md <md@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2007-10-30 05:12:27 +0000

    Mit make_oid erstellte Nummern haben jetzt eine Verhoeff Prüfziffer.
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/trunk/web/MoftS/lib/myPL/kernelE@2475 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- src/mypl_nveserver.erl
+++ src/mypl_nveserver.erl
@@ -30,9 +30,19 @@ start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 make_nve() ->
+    % start the server if it is not already running
+    case whereis(?SERVER) of
+        undefined -> start_link();
+        _ -> ok
+    end,
     gen_server:call(?SERVER, {make_nve}).
 
 make_oid() ->
+    % start the server if it is not already running
+    case whereis(?SERVER) of
+        undefined -> start_link();
+        _ -> ok
+    end,
     gen_server:call(?SERVER, {make_oid}).
 
 %%====================================================================
@@ -111,6 +121,8 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%--------------------------------------------------------------------
 
+% @doc calculate a EAN check digit
+% verify at http://www.gs1.org/productssolutions/barcodes/support/check_digit_calculator.html
 ean_digit(Nums) ->
     {_, Summe} = 
     lists:foldr(fun(X, {Factor, Summe}) -> 
@@ -118,22 +130,101 @@ ean_digit(Nums) ->
                 end, {3, 0}, Nums),
     [$0 + ((10-(Summe rem 10)) rem 10)].
 
+
+% dihedral addition matrix A+B = a[A][B] used by verhoeff_digit()
+-define(DIHEDRAL_A, {{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                     { 1, 2, 3, 4, 0, 6, 7, 8, 9, 5 },
+                     { 2, 3, 4, 0, 1, 7, 8, 9, 5, 6 },
+                     { 3, 4, 0, 1, 2, 8, 9, 5, 6, 7 },
+                     { 4, 0, 1, 2, 3, 9, 5, 6, 7, 8 },
+                     { 5, 9, 8, 7, 6, 0, 4, 3, 2, 1 },
+                     { 6, 5, 9, 8, 7, 1, 0, 4, 3, 2 },
+                     { 7, 6, 5, 9, 8, 2, 1, 0, 4, 3 },
+                     { 8, 7, 6, 5, 9, 3, 2, 1, 0, 4 },
+                     { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }}).
+-define(DIHEDRAL_INVERSE, {0, 4, 3, 2, 1, 5, 6, 7, 8, 9}).
+-define(DIHEDRAL_P, { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                      { 1, 5, 7, 6, 2, 8, 3, 0, 9, 4 },
+                      { 5, 8, 0, 3, 7, 9, 6, 1, 4, 2 },
+                      { 8, 9, 1, 6, 0, 4, 3, 5, 2, 7 },
+                      { 9, 4, 5, 3, 1, 2, 6, 8, 7, 0 },
+                      { 4, 2, 8, 6, 5, 7, 3, 9, 0, 1 },
+                      { 2, 7, 9, 3, 8, 0, 6, 4, 1, 5 },
+                      { 7, 0, 4, 6, 9, 1, 3, 2, 5, 8 }}).
+
+verhoeff_digit_helper(Char, {Pos, X}) ->
+    Pval = element((Char-$0)+1, element(((Pos+1) rem 8)+1, ?DIHEDRAL_P)),
+    XNew = element(Pval+1, element(X+1, ?DIHEDRAL_A)),
+    {Pos + 1, XNew}.
+    
+
+%% @doc generate a Verhoeff Check Digit
+%% 
+%% See http://en.wikipedia.org/wiki/Verhoeff_algorithm
+verhoeff_digit(NumStr) ->
+    % There are various (contradicting) implementation guidelines for the Verhoeff algorithm:
+    % http://www.cs.utsa.edu/~wagner/pubs/DecimalDigits.pdf
+    % http://static.23.nu/md/Pictures/SNOMEDCT_Core_Technical+Reference+Manual_US.pdf
+    % http://static.23.nu/md/Pictures/adfa.pdf
+    % http://www.cs.utsa.edu/~wagner/laws/verhoeff.html
+    % http://en.wikipedia.org/wiki/Verhoeff_algorithm
+    % http://www.vuemetrix.com/vueseries/comm_protocols.html
+    % http://en.dahnielson.com/2006/09/verhoeff.html
+    % http://www.ams.org/featurecolumn/archive/barcodes6.html
+    % see voerhoeff_test() for some examples of contradicting results.
+    {_, X} = lists:foldl(fun(Char, Acc) -> verhoeff_digit_helper(Char, Acc) end,
+                        {0, 0}, lists:reverse(NumStr)),
+    [$0 + element(X+1, ?DIHEDRAL_INVERSE)].
+
 pad_helper(Howmuch, L) when Howmuch > 0 ->
     pad_helper(Howmuch - 1, [$0|L]);
 pad_helper(_Howmuch, L) -> L.
 
+
+%% @doc 0-pad a string
 pad(Howmuch, L) ->
     pad_helper(Howmuch-length(L), L).
+
+%% @doc generate a NVE/SSCC from
+%%
+%% This are generated from HUDORA's pool of NVEs keeping internal state to ensure no duplicate NVEs
+%% are generated.
+%% 
+%% ```1> mypl_nveserver:make_nve().
+%% "340059981000000005"
+%% 2> mypl_nveserver:make_nve().
+%% "340059981000000012"
+%% 3> mypl_nveserver:make_nve().
+%% "340059981000000029"'''
+
 
 make_nve(Num) ->
     Prefix = "340059981",
     NveRaw = Prefix ++ pad(8, erlang:integer_to_list(Num)),
     NveRaw ++ ean_digit(NveRaw).
     
+
+%% @doc generate a unique number
+%%
+%% This numbers have the propertiey that they sort according to sequence of creation and they are
+%% ending with a Verhoeff Check Digit. You can simply ignore that fact but you also can use it
+%% to verify it during data entry.
+%%
+%% ```1> mypl_nveserver:make_oid()
+%% "00000001"
+%% 2> mypl_nveserver:make_oid().
+%% "00000017"
+%% 3> mypl_nveserver:make_oid().
+%% "00000029"
+%% 4> mypl_nveserver:make_oid().
+%% "00000038"'''
+
+
 make_oid(Num) ->
-    "Z" ++ pad(7, erlang:integer_to_list(Num)).
+    NumStr = pad(7, erlang:integer_to_list(Num)),
+    NumStr ++ verhoeff_digit(NumStr).
     
-    
+
 checkpoint_file() -> mnesia:system_info(directory) ++ "/nveserver_checkpoint".
 
 % reads state file from disk
@@ -185,7 +276,60 @@ check_checkpoint(State) ->
 -ifdef(EUNIT).
 -compile(export_all).
     
-testrunner() ->
+verhoeff_test() ->
+    % Different Implementations of Verhoeff's Dihedral Check Digit seem to produce different results.
+    % We stick to the way of calculating implemented by Perl's Algorithm::Verhoeff,
+    % http://www.augustana.ab.ca/~mohrj/algorithms/checkdigit.html and 
+    % http://en.wikipedia.org/wiki/Verhoeff_algorithm
+    % The Implementation of http://code.google.com/p/checkdigits/ sems not to reverse the input before
+    % calculation which seems to be the only difference.
+    
+    % http://www.esb.ie/esbnetworks/mrso/mprn.jsp
+    % [$9] = verhoeff_digit("10003729"),
+    
+    % http://web.mit.edu/kenta/www/three/verhoeff-checksum.html.gz
+    % [$8] = verhoeff_digit("1125"),
+    % [$5] = verhoeff_digit("16412"),
+    
+    % http://www.vuemetrix.com/vueseries/comm_protocols.html
+    % [$2] = verhoeff_digit("0000168"),
+    % [$1] = verhoeff_digit("04052"),
+    % [$9] = verhoeff_digit("1"),
+    
+    % Perl's Algorithm::Verhoeff test code
+    [$9] = verhoeff_digit("123456654321"),
+    [$3] = verhoeff_digit("5743839105748193475681981039847561718657489228374"),
+    
+    % http://en.wikipedia.org/wiki/Verhoeff_algorithm
+    [$0] = verhoeff_digit("142857"),
+    
+    % http://www.augustana.ab.ca/~mohrj/algorithms/checkdigit.html
+    [$5] = verhoeff_digit("1"),
+    [$3] = verhoeff_digit("11"),
+    [$3] = verhoeff_digit("050"),
+    [$3] = verhoeff_digit("505"),
+    [$1] = verhoeff_digit("494"),
+    [$1] = verhoeff_digit("949"),
+    [$5] = verhoeff_digit("272"),
+    [$5] = verhoeff_digit("727"),
+    [$8] = verhoeff_digit("161"),
+    [$8] = verhoeff_digit("616"),
+    [$4] = verhoeff_digit("383"),
+    [$9] = verhoeff_digit("838"),
+    [$2] = verhoeff_digit("1125"),
+    [$6] = verhoeff_digit("04052"),
+    [$8] = verhoeff_digit("16412"),
+    [$2] = verhoeff_digit("505505"),
+    [$4] = verhoeff_digit("050050"),
+    [$0] = verhoeff_digit("142857"),
+    [$4] = verhoeff_digit("1456789"),
+    [$6] = verhoeff_digit("0000168"),
+    [$1] = verhoeff_digit("10003729"),
+    [$9] = verhoeff_digit("123456654321"),
+    [$3] = verhoeff_digit("5743839105748193475681981039847561718657489228374"),
+    ok.
+    
+ean_test() ->
     [$0] = ean_digit(""),
     [$4] = ean_digit("2"), 
     [$2] = ean_digit("22"), 
@@ -253,6 +397,12 @@ testrunner() ->
     [$5] = ean_digit("999999999999999999999999999999999999999999999999999999999"), 
     [$6] = ean_digit("9999999999999999999999999999999999999999999999999999999999"),
     [$9] = ean_digit("99999999999999999999999999999999999999999999999999999999999"),
+    
+    ok.
+
+testrunner() ->
+    verhoeff_test(),
+    ean_test(),
     ok.
 
 -endif.
