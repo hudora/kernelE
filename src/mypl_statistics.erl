900d44b3396f1adab9c7c4cd4a1f435a8b74a906
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-02-12 23:59:15 +0000

    hourly statistics
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/trunk/web/MoftS/lib/myPL/kernelE@3060 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- src/mypl_statistics.erl
+++ src/mypl_statistics.erl
@@ -1,6 +1,6 @@
 -module(mypl_statistics).
 
--export([statistics/0, aggregate/0]).
+-export([statistics/0, aggregate/0, aggregate2/0]).
 
 -include_lib("stdlib/include/qlc.hrl").
 -include("include/mypl.hrl").
@@ -15,6 +15,20 @@ aggregate() ->
     Records = mypl_db_util:do_trans(qlc:q([X || X <- mnesia:table(archive)])),
     lists:sort(dict:to_list(aggregate_helper(Records, dict:new()))).
     
+
+
+aggregate2_helper([], Dict) -> Dict;
+aggregate2_helper([Record|Tail], Dict) ->
+    Date = element(1, Record#archive.created_at),
+    Hour = element(1, element(2, Record#archive.created_at)),
+    aggregate2_helper(Tail, dict:update_counter({{Date, {Hour, 0, 0}}, Record#archive.archived_by}, 1, Dict)).
+    
+
+aggregate2() ->
+    Records = mypl_db_util:do_trans(qlc:q([X || X <- mnesia:table(archive)])),
+    lists:reverse(lists:sort(dict:to_list(aggregate2_helper(Records, dict:new())))).
+    
+
 statistics() ->
     [{empty_pickable_locations, mypl_movements:count_empty_floor_locations()},
      {multi_floorunits,         mypl_movements:more_than_one_floorunit()},
