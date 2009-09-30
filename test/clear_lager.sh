cf2effcae56da0d1b0ccde38c720e6fb0c1fdcab
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-09-30 22:38:24 +0000

    cleaned up VERY OLD tests
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/kernelE/trunk@6657 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- test/clear_lager.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# empties myPL
-rm -Rf Mnesia.mypl_test@*
-erl -pa ./ebin -sname mypl_test -noinput -s mypl_db run_me_once -s erlang halt
--- test/start_testserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-erl -pa ./ebin -sname mypl_test -setcookie xTESTx \
-    -s mnesia start -s mnesia load_textfile "\"test/data/platzstammdaten.mnesiadump\"" -s tv start -s mypl start
