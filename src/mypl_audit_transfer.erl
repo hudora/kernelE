62c19c52df4d39d9c0d6cb95a8f53fcab8ca484c
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-10-11 22:42:04 +0000

    cleanup
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/kernelE/trunk@6732 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- src/mypl_audit_transfer.erl
+++ src/mypl_audit_transfer.erl
@@ -37,7 +37,7 @@ proplist_cleanup_binary([H|T]) ->
     [H|proplist_cleanup_binary(T)].
 
 save_into_couchdb(DbName, Id, Doc) ->
-    case erlang_couchdb:create_document({"couchdb.local.hudora.biz", 5984}, DbName, Id, Doc) of
+    case erlang_couchdb:q({"couchdb.local.hudora.biz", 5984}, DbName, Id, Doc) of
         {json,{[{<<"ok">>,true}|_]}} -> 
             ok;
         {json,{[{<<"error">>,<<"conflict">>}, {<<"reason">>,<<"Document update conflict.">>}]}} ->
@@ -67,89 +67,19 @@ transfer_archive(Key) ->
             ArchivedAt = mypl_util:timestamp2binary(Record#archive.created_at),
             case {Record#archive.type, size(Record#archive.body)} of
                 {pick, 7} ->
-                    save_into_couchdb("mypl_archive",
-                             Body#pick.id ++ "-" ++ Record#archive.id,
-                             [{type, <<"pick">>},
-                              {mui, mypl_util:ensure_binary(Body#pick.from_unit)},
-                              {quantity, Body#pick.quantity},
-                              {product, mypl_util:ensure_binary(Body#pick.product)},
-                              {attributes, {proplist_cleanup_binary(Body#pick.attributes)}},
-                              {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
-                              {archived_at, ArchivedAt},
-                              {created_at, mypl_util:timestamp2binary(Body#pick.created_at)}
-                             ]),
-                    ok = mnesia:dirty_delete(archive, Key),
+                    save_pick(Key, Record, Body, ArchivedAt),
                     transfer_archive(NextKey);
                 {movement, 7} ->
-                    save_into_couchdb("mypl_archive",
-                             Body#movement.id ++ "-" ++ Record#archive.id,
-                             [{type, <<"movement">>},
-                              {mui, mypl_util:ensure_binary(Body#movement.mui)},
-                              {from_location, mypl_util:ensure_binary(Body#movement.from_location)},
-                              {to_location, mypl_util:ensure_binary(Body#movement.to_location)},
-                              {attributes, {proplist_cleanup_binary(Body#movement.attributes)}},
-                              {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
-                              {archived_at, ArchivedAt},
-                              {created_at, mypl_util:timestamp2binary(Body#movement.created_at)}
-                             ]),
-                    ok = mnesia:dirty_delete(archive, Key),
+                    save_movement(Key, Record, Body, ArchivedAt),
                     transfer_archive(NextKey);
                 {unit, 9} ->
-                    save_into_couchdb("mypl_archive",
-                            Body#unit.mui ++ "-" ++ Record#archive.id,
-                            [{type, <<"unit">>},
-                             {mui, mypl_util:ensure_binary(Body#unit.mui)},
-                             {quantity, Body#unit.quantity},
-                             {product, mypl_util:ensure_binary(Body#unit.product)},
-                             {attributes, {proplist_cleanup_binary(Body#unit.attributes)}},
-                             {location, mypl_util:ensure_binary(Body#unit.location)},
-                             {height, Body#unit.height},
-                             {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
-                             {archived_at, ArchivedAt},
-                             {created_at, mypl_util:timestamp2binary(Body#unit.created_at)}
-                            ]),
-                    ok = mnesia:dirty_delete(archive, Key),
+                    save_unit(Key, Record, Body, ArchivedAt),
                     transfer_archive(NextKey);
                 {provpipeline, 10} ->
-                    erlang:display(Body),
-                    save_into_couchdb("mypl_archive",
-                            Body#provpipeline.id ++ "-" ++ Record#archive.id,
-                            [{type, <<"provpipeline">>},
-                             {id, mypl_util:ensure_binary(Body#provpipeline.id)},
-                             {priority, Body#provpipeline.priority},
-                             {attributes, {proplist_cleanup_binary(Body#provpipeline.attributes) 
-                                           ++ [{weigth, Body#provpipeline.weigth}, {volume, Body#provpipeline.volume}]}},
-                             {status, mypl_util:ensure_binary(Body#provpipeline.status)},
-                             {tries, Body#provpipeline.tries},
-                             {provisioninglists, Body#provpipeline.provisioninglists},
-                             {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
-                             {archived_at, ArchivedAt},
-                             {orderlines, lists:map(fun({Quantity, Product, Attributes}) -> 
-                                                        [Quantity, mypl_util:ensure_binary(Product), {proplist_cleanup_binary(Attributes)}]
-                                                    end, Body#provpipeline.orderlines)}
-                            ]),
-                    ok = mnesia:dirty_delete(archive, Key),
+                    save_provpipeline(Key, Record, Body, ArchivedAt),
                     transfer_archive(NextKey);
                 {provisioninglist, 10} ->
-                    save_into_couchdb("mypl_archive",
-                            Body#provisioninglist.id ++ "-" ++ Record#archive.id,
-                            [{type, Body#provisioninglist.type},
-                             {provpipeline_id, mypl_util:ensure_binary(Body#provisioninglist.provpipeline_id)},
-                             {destination, mypl_util:ensure_binary(Body#provisioninglist.destination)},
-                             {attributes, {proplist_cleanup_binary(Body#provisioninglist.attributes)
-                                           ++ [{parts, Body#provisioninglist.parts}]}},
-                             {provisionings, lists:map(fun({Id, _, _, _, _, _}) ->
-                                                           mypl_util:ensure_binary(Id);
-                                                          (Id) ->
-                                                           mypl_util:ensure_binary(Id)
-                                                       end,
-                                                       Body#provisioninglist.provisionings)},
-                             {status, mypl_util:ensure_binary(Body#provisioninglist.status)},
-                             {created_at, mypl_util:ensure_binary(Body#provisioninglist.created_at)},
-                             {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
-                             {archived_at, ArchivedAt}
-                             ]),
-                    ok = mnesia:dirty_delete(archive, Key),
+                    save_provisioninglist(Key, Record, Body, ArchivedAt),
                     transfer_archive(NextKey);
                 Other ->
                     erlang:display(Record#archive.body),
@@ -158,7 +88,96 @@ transfer_archive(Key) ->
                     erlang:display({vv3, Body})
             end
     end.
+    
 
+save_pick(Key, Record, Body, ArchivedAt) ->
+    save_into_couchdb("mypl_archive",
+         Body#pick.id ++ "-" ++ Record#archive.id,
+         [{type, <<"pick">>},
+          {mui, mypl_util:ensure_binary(Body#pick.from_unit)},
+          {quantity, Body#pick.quantity},
+          {product, mypl_util:ensure_binary(Body#pick.product)},
+          {attributes, {proplist_cleanup_binary(Body#pick.attributes)}},
+          {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
+          {archived_at, ArchivedAt},
+          {created_at, mypl_util:timestamp2binary(Body#pick.created_at)}
+         ]),
+    ok = mnesia:dirty_delete(archive, Key).
+    
+
+save_movement(Key, Record, Body, ArchivedAt) ->
+    save_into_couchdb("mypl_archive",
+         Body#movement.id ++ "-" ++ Record#archive.id,
+         [{type, <<"movement">>},
+          {mui, mypl_util:ensure_binary(Body#movement.mui)},
+          {from_location, mypl_util:ensure_binary(Body#movement.from_location)},
+          {to_location, mypl_util:ensure_binary(Body#movement.to_location)},
+          {attributes, {proplist_cleanup_binary(Body#movement.attributes)}},
+          {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
+          {archived_at, ArchivedAt},
+          {created_at, mypl_util:timestamp2binary(Body#movement.created_at)}
+         ]),
+    ok = mnesia:dirty_delete(archive, Key).
+    
+
+save_unit(Key, Record, Body, ArchivedAt) ->
+    save_into_couchdb("mypl_archive",
+            Body#unit.mui ++ "-" ++ Record#archive.id,
+            [{type, <<"unit">>},
+             {mui, mypl_util:ensure_binary(Body#unit.mui)},
+             {quantity, Body#unit.quantity},
+             {product, mypl_util:ensure_binary(Body#unit.product)},
+             {attributes, {proplist_cleanup_binary(Body#unit.attributes)}},
+             {location, mypl_util:ensure_binary(Body#unit.location)},
+             {height, Body#unit.height},
+             {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
+             {archived_at, ArchivedAt},
+             {created_at, mypl_util:timestamp2binary(Body#unit.created_at)}
+            ]),
+    ok = mnesia:dirty_delete(archive, Key).
+    
+
+save_provpipeline(Key, Record, Body, ArchivedAt) ->
+    erlang:display(Key, Body),
+    save_into_couchdb("mypl_archive",
+            Body#provpipeline.id ++ "-" ++ Record#archive.id,
+            {[{type, <<"provpipeline">>},
+              {id, mypl_util:ensure_binary(Body#provpipeline.id)},
+              {priority, Body#provpipeline.priority},
+              {attributes, {proplist_cleanup_binary(Body#provpipeline.attributes) 
+                            ++ [{weigth, Body#provpipeline.weigth}, {volume, Body#provpipeline.volume}]}},
+              {status, mypl_util:ensure_binary(Body#provpipeline.status)},
+              {tries, Body#provpipeline.tries},
+              {provisioninglists, Body#provpipeline.provisioninglists},
+              {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
+              {archived_at, ArchivedAt},
+              {orderlines, lists:map(fun({Quantity, Product, Attributes}) -> 
+                                         [Quantity, mypl_util:ensure_binary(Product), {proplist_cleanup_binary(Attributes)}]
+                                     end, Body#provpipeline.orderlines)}
+             ]}),
+    ok = mnesia:dirty_delete(archive, Key).
+    
+
+save_provisioninglist(Key, Record, Body, ArchivedAt) ->
+    save_into_couchdb("mypl_archive",
+        Body#provisioninglist.id ++ "-" ++ Record#archive.id,
+        [{type, Body#provisioninglist.type},
+         {provpipeline_id, mypl_util:ensure_binary(Body#provisioninglist.provpipeline_id)},
+         {destination, mypl_util:ensure_binary(Body#provisioninglist.destination)},
+         {attributes, {proplist_cleanup_binary(Body#provisioninglist.attributes)
+                       ++ [{parts, Body#provisioninglist.parts}]}},
+         {provisionings, lists:map(fun({Id, _, _, _, _, _}) ->
+                                       mypl_util:ensure_binary(Id);
+                                      (Id) ->
+                                       mypl_util:ensure_binary(Id)
+                                   end,
+                                   Body#provisioninglist.provisionings)},
+         {status, mypl_util:ensure_binary(Body#provisioninglist.status)},
+         {created_at, mypl_util:ensure_binary(Body#provisioninglist.created_at)},
+         {archived_by, mypl_util:ensure_binary(Record#archive.archived_by)},
+         {archived_at, ArchivedAt}
+         ]),
+    ok = mnesia:dirty_delete(archive, Key).
 
 %% @doc Transfer Unit Audit data to Postgresql.
 transfer_unitaudit() ->
@@ -217,12 +236,5 @@ transfer_articleaudit(Key) ->
 
 % @doc spawn start_transfer/0 - but ensure only one is running
 spawn_audit_transfer() ->
-    {{_Year, _Month, _Day}, {Hour, _Minutes, _Seconds}} = erlang:localtime(),
-    % ony spawn at 2h
-    case Hour of
-        2 ->
-            mypl_util:spawn_and_register(audit_transfer_process, fun() -> transfer_all() end);
-        _ -> 
-            ok
-    end,
+    mypl_util:spawn_and_register(audit_transfer_process, fun() -> transfer_all() end),
     ok.
