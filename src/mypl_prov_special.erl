d752cf465538359ae69bac7e14774093502dfb4f
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2010-01-28 08:32:20 +0000

    changes found on hurricane
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/kernelE/trunk@7527 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- src/mypl_prov_special.erl
+++ src/mypl_prov_special.erl
@@ -129,7 +129,7 @@ flood_requestracker() ->
 flood_requestracker([]) -> ok;
 flood_requestracker([Entry|CandidatesTail]) ->
     Orderlines = [{Quantity, Product} || {Quantity, Product, _Attributes} <- Entry#provpipeline.orderlines],
-    mypl_choose:find_provisioning_candidates_multi(Orderlines, mypl_prov_util:sort_provpipeline_helper(Entry)),
+    mypl_choose:find_provisioning_candidates_multi(Orderlines, {[{priority, mypl_prov_util:sort_provpipeline_helper(Entry)}]}),
     flood_requestracker(CandidatesTail).
     
 
